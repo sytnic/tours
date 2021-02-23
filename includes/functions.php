511afa2301cd92c7b5c27d20e2813b5e164f463d
sytnic <sytnic@list.ru>
2021-02-23 22:56:45 +0300

    Before as unix_to_date in form_read

--- includes/functions.php
+++ includes/functions.php
@@ -5,18 +5,49 @@
 		if (!$result_set){
 		die("Database query failed. Function confirm_query failed or its using in others func-s failed.");}
 	}
-
-function find_all_pages() { 
+	
+function find_courier_by_id($got_id) { 
 		global $connection;
-		// 2. Perform database query
+		
+		$safe_got_id = mysqli_real_escape_string($connection, $got_id);
+		
 		$query  = "SELECT * ";
-		$query .= "FROM pages ";
-			$query .= "WHERE visible = 1 ";	
-		$query .= "ORDER BY number ASC";
-		$page_set = mysqli_query($connection, $query);
+		$query .= " FROM couriers";
+		$query .= " WHERE id = {$safe_got_id} ";
+		$query .= " LIMIT 1";
+		$courier_set = mysqli_query($connection, $query);
 		// Test if there was a query error
-		confirm_query($page_set);
-		return $page_set;
+		confirm_query($courier_set);
+		
+		if ($courier = mysqli_fetch_assoc($courier_set)) {
+			return $courier; // $courier == $courier_array as row from db
+		} else {
+			return null;
+		}		
 	}
+	
+function find_region_by_id($got_id) { 
+		global $connection;
+		
+		$safe_got_id = mysqli_real_escape_string($connection, $got_id);
+		
+		$query  = "SELECT * ";
+		$query .= " FROM regions";
+		$query .= " WHERE id = {$safe_got_id} ";
+		$query .= " LIMIT 1";
+		$region_set = mysqli_query($connection, $query);
+		// Test if there was a query error
+		confirm_query($region_set);
+		
+		if ($region = mysqli_fetch_assoc($region_set)) {
+			return $region; // $region == $region_array as row from db
+		} else {
+			return null;
+		}		
+	}
+	
+function unix_to_date($unix_time) {
+	return strftime("%d.%m.%y", $unix_time);	
+}
 	
 	
